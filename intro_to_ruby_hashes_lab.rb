c0679b3029699f5d0f6efd57548bc2435aa62810
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-20 19:18:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_third_tier
       },
       names: {
         reading_railroad: {},
-        pennsylvainia_railroad: {},
+        pennsylvania_railroad: {},
         b_and_o_railroad: {},
         shortline_railroad: {}
       }
@@ -65,7 +65,7 @@ def monopoly_with_fourth_tier
         reading_railroad: {
           mortgage_value: 100
         },
-        pennsylvainia_railroad: {
+        pennsylvania_railroad: {
           mortgage_value: 200
         },
         b_and_o_railroad: {
