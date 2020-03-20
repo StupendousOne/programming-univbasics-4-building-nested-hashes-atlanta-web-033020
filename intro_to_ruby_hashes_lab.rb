bbc2b9b506a279126ef731693eae0657f25e50d6
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-20 19:16:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_second_tier
   new_hash = {
     railroads: {
       pieces: 4,
-      rents: {},
+      rent_in_dollars: {},
       names: {}
     }
   }
@@ -30,8 +30,18 @@ def monopoly_with_third_tier
   new_hash = {
     railroads: {
       pieces: 4,
-      rents: {},
-      names: {}
+      rents: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200
+      },
+      names: {
+        reading_railroad: {},
+        pennsylvainia_railroad: {},
+        b_and_o_railroad: {},
+        shortline_railroad: {}
+      }
     }
   }
   new_hash
@@ -42,5 +52,24 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  new_hash = {
+    railroads: {
+      pieces: 4,
+      rents: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200
+      },
+      names: {
+        reading_railroad: {
+          mortgage_value: 100
+        },
+        pennsylvainia_railroad: {},
+        b_and_o_railroad: {},
+        shortline_railroad: {}
+      }
+    }
+  }
+  new_hash
 end
